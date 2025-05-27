18ae5f96e4afff03b9ed6af031d2ba692775f780
Nicolas Filippelli <nicolasfilippelli22@gmail.com>
2025-05-26 23:18:09 -0300

    Added base project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AnimalParkReimagined.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
